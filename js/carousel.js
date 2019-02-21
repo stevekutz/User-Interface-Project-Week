8c53ef3d8349b3c10e1e136f56473b573fa24111
stevekutz <stkutz@gmail.com>
2019-02-21 00:04:01 -0600

    minor code cleanup

--- js/carousel.js
+++ js/carousel.js
@@ -1,55 +1,38 @@
 class Carousel {
   constructor( carouselElement ) {
-      // console.log('###########  inside Carousel constructor');
     this.carouselElement = carouselElement;
-      // console.log('carouselElement is ', this.carouselElement);
 
     this.carouselImages = carouselElement.querySelectorAll('img');
-      // console.log('this carousel images  ', this.carouselImages);
-      // console.log('length of this.carouseImages is ', this.carouselImages.length);
 
     this.currentIndex = 0;
     this.maxIndex = this.carouselImages.length - 1;
     this.currentImage = this.carouselImages[this.currentIndex];
     this.currentImage.style.display = 'flex';
 
-
-    // need reference to left & right
+    // need reference to left & right btns
     this.left_btn = this.carouselElement.querySelector('.left-button');
     this.right_btn = this.carouselElement.querySelector('.right-button');
 
     // build event listeners for left & right button divs
     this.left_btn.addEventListener('click', () => this.left() );
     this.right_btn.addEventListener('click', () => this.right() );
-
-
   }
 
   left() {
     navBTNs[this.currentIndex].classList.toggle('active');
     btnInfoStatus[this.currentIndex].classList.toggle('active');
 
-    console.log('LLLLL  inside of left, currentIndex ORIGINALLY ', this.currentIndex);
-    console.log('LLL  Number(this.currentIndex) is ', Number(this.currentIndex)) ;
-    console.log('LLLLL  this.maxIndex is ', this.maxIndex);
-
-
     Number(this.currentIndex) === 0
       ? this.currentIndex = this.maxIndex
       : this.currentIndex = Number(this.currentIndex) - 1;
-
-       console.log('LLLLL inside of left, currentIndex changed to ', this.currentIndex);
-       console.log('LLLLL navBTNs is ', navBTNs);
 
     navBTNs[this.currentIndex].classList.toggle('active');
     btnInfoStatus[this.currentIndex].classList.toggle('active');
 
     this.showImage();
-
   }
 
   right() {
-    //  OMG this works !!
     navBTNs[this.currentIndex].classList.toggle('active');
     btnInfoStatus[this.currentIndex].classList.toggle('active');
 
@@ -61,7 +44,6 @@ class Carousel {
        console.log('inside of right, currentIndex changed to ', this.currentIndex);
        console.log('navBTNs is ', navBTNs);
 
-      //   OMG this works !!
      navBTNs[this.currentIndex].classList.toggle('active');
      btnInfoStatus[this.currentIndex].classList.toggle('active');
 
@@ -72,13 +54,11 @@ class Carousel {
     this.currentImage.style.display = 'none';
     this.currentImage = this.carouselImages[this.currentIndex];
     this.currentImage.style.display = 'flex';
-
   }
 
 }
 
 let carouselDOM = document.querySelector('.carousel');
-//console.log('~~~~~~~~~~~  carousel gives back ', carouselDOM);
 
 let carousel = new Carousel(carouselDOM);
 
@@ -86,31 +66,23 @@ let carousel = new Carousel(carouselDOM);
 class NavButton {
   constructor(navBTN_attrib) {
     this.navBTN = navBTN_attrib;  // entire nav_divBTN div for each img
-      console.log('++++++  navTBN created with attr of ', this.navBTN);
 
     // dataset attribute for navBTN  - DOMStringMap
     this.navBTN_data_img = this.navBTN.dataset.img;
-         console.log('divBTN img dataset value is', this.navBTN_data_img);
 
     //  match BTN-info div to navBTN dataset value
     this.navBTN_content = document
       .querySelector(`.BTN-info[data-img = "${this.navBTN_data_img}"]`);
-      // console.log('@@@@@@  this.navBTN.content is ', this.navBTN_content);
-
-
 
     this.navBTN_content = new NavInfoContent(this.navBTN_content);
 
     this.navBTN.addEventListener('click', () => this.selectNavBTN());
 
-
   }
-
 
 
   selectNavBTN() {
     this.navBTN_content.toggleActiveNavBTN();
-      //console.log('!!!!! this.navBTN is ', this.navBTN);
 
     // toggle active nav control
     const navBTN_status = document.querySelectorAll('.nav_divBTN');
@@ -120,11 +92,6 @@ class NavButton {
     // set current index to currently selected data-img value
     carousel.currentIndex = this.navBTN_data_img;
     carousel.showImage();
-
-
-    console.log('######carousel current index is ', carousel.currentIndex);
-
-
   }
 }
 
@@ -144,15 +111,10 @@ class NavInfoContent {
 
 
 
-// reference to nav_tabBTN
+// reference to nav control buttons
 const navBTNs = document.querySelectorAll('.nav_divBTN');
-  // console.log('navBTN QSA is ', navBTNs);
-
-
-navBTNStatus = Array.from(navBTNs);
-console.log('****** navBTNStatus', navBTNStatus);
-
 
 [...navBTNs].forEach(item => new NavButton(item) );
 
+// reference to nav btn content
 const btnInfoStatus = document.querySelectorAll('.BTN-info');
